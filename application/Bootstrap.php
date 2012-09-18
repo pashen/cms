498b4a467a51a6222bbdc788c418c280bbcce0b2
pashen <pashen87@gmail.com>
2012-09-18 17:02:42 +0300

    add skin feature

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -2,11 +2,19 @@
 
 class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
 {
-
+  // Iniialize view
   function _initView()
   {
+    $view = new Zend_View();
+    $view->doctype('XHTML1_STRICT');
+    $view->headTitle('Zend CMS');
+    $view->skin = 'blues';
     
-    
+    // Add it to the ViewRenderer
+    $viewRenderer = Zend_Controller_Action_HelperBroker::getStaticHelper('ViewRenderer');
+    $viewRenderer->setView($view);
+    // Return it so it can be stored by the bootstrap
+    return $view;
   }
 }
 
